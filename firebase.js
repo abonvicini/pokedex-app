01f5f1b1a7ce1dc04495866fc79ffbbb3206ae2d
Alexis Ivan Bonvicini <alexisbonvicini@gmail.com>
2022-11-15 01:15:21 -0300

    add react-native-env

--- firebase.js
+++ firebase.js
@@ -1,10 +1,20 @@
 // Your web app's Firebase configuration
 //TODO: consumir por variables de entorno
+
+import {
+    API_KEY,
+    AUTH_DOMAIN,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+    MESSAGING_SENDER_ID,
+    APP_ID,
+} from '@env';
+
 export const firebaseConfig = {
-    apiKey: 'AIzaSyAtT8D0SKxkPJKdzZ_1fnPC6gyG_BGrr7E',
-    authDomain: 'pokedex-app-cf4ad.firebaseapp.com',
-    projectId: 'pokedex-app-cf4ad',
-    storageBucket: 'pokedex-app-cf4ad.appspot.com',
-    messagingSenderId: '1058914300368',
-    appId: '1:1058914300368:web:e6afa60642ed4f04d289d5',
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
 };
