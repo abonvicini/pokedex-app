4492e62f23f32d25d4383378742ef26fbfa86e09
Alexis Ivan Bonvicini <alexisbonvicini@gmail.com>
2022-11-20 02:17:06 -0300

    se modifica el maxCalls

--- hooks/usePokeSetUp.jsx
+++ hooks/usePokeSetUp.jsx
@@ -9,7 +9,7 @@ export const usePokeSetUp = () => {
     React.useEffect(() => {
         console.log(statusSetUp);
 
-        recursiveSequentialFetch({ id: 1, data: [], maxCalls: 90 })
+        recursiveSequentialFetch({ id: 1, data: [], maxCalls: 906 })
             .then((data) => {
                 // console.log('Sequential Fetch Done', data.data);
                 setPokeData(data.data);
